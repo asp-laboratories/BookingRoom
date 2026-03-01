3c50e1d6bcaae366244710fb3ec894cf63c65e6f
LuisDavidGR <ldavidgrdrmz@gmail.com>
2026-03-01 13:04:08 -0800

    feat: Integrando archivos de html al proyecto, entendiendo la estructura

--- BookingRoomApp/views.py
+++ BookingRoomApp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def login(request):
+    return render(request, 'BookingRoomApp/login.html')
+
+def sign_up(request):
+    return render(request, 'BookingRoomApp/sign_up.html')
