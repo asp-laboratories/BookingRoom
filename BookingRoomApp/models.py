c826240a94ec1652d54033cbf6a6b7344dc2b021
LuisDavidGR <ldavidgrdrmz@gmail.com>
2026-03-19 18:08:43 -0700

    feat: Creando todos los modelos necesarios para el login y configuracion para trabajar local en SQLite(debug=False) - 02

--- BookingRoomApp/models.py
+++ BookingRoomApp/models.py
@@ -16,5 +16,79 @@ class TipoServicio(models.Model):
     def __str__(self):
         return self.nombre
 
+class Rol(models.Model):
+    codigo = models.CharField(max_length=5, primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'rol'
+        verbose_name = "Rol"
+
+class EstadoCuenta(models.Model):
+    codigo = models.CharField(max_length=5, primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'estado_cuenta'
+        verbose_name = "Estado de cuenta"
+
+class TipoCliente(models.Model):
+    codigo = models.CharField(max_length=5, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    disposicion = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'tipo_cliente'
+        verbose_name = "Tipo de cliente"
+
+
+class Cuenta(models.Model):
+    nombre_usuario = models.CharField(max_length=100)
+    correo_electronico = models.EmailField()
+    disposicion = models.BooleanField(default=True)
+    estado_cuenta = models.ForeignKey(EstadoCuenta, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'cuenta'
+        verbose_name = "Cuenta"
+
+class Trabajador(models.Model):
+    no_empleado = models.CharField(max_length=10, primary_key=True)
+    rfc = models.CharField(max_length=13)
+    nombre_fiscal = models.CharField(max_length=150)
+    nombre = models.CharField(max_length=150)
+    apellidoPaterno = models.CharField(max_length=150)
+    apelidoMaterno = models.CharField(max_length=150)
+    telefono = models.CharField(max_length=15)
+    correo_electronico = models.EmailField()
+    rol = models.ForeignKey(Rol, on_delete=models.PROTECT)
+    cuenta = models.ForeignKey(Cuenta, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'trabajador'
+        verbose_name = "Trabajador"
+
+class DatosCliente(models.Model):
+    rfc = models.CharField(max_length=13)
+    nombre_fiscal = models.CharField(max_length=150)
+    nombre = models.CharField(max_length=150)
+    apellidoPaterno = models.CharField(max_length=150)
+    apelidoMaterno = models.CharField(max_length=150)
+    telefono = models.CharField(max_length=15)
+    correo_electronico = models.EmailField()
+    dir_colonia = models.CharField(max_length=150)
+    dir_calle = models.CharField(max_length=150)
+    dir_numero = models.CharField( max_length=50)
+    tipo_cliente = models.ForeignKey(TipoCliente, on_delete=models.PROTECT)
+    cuenta = models.ForeignKey(Cuenta, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'datos_cliente'
+        verbose_name = "Datos del cliente"
+        
+
+
+
+
     # def get_absolute_url(self):
     #     return reverse("TipoServicio_detail", kwargs={"pk": self.pk})
--- BookingRoom_Django/settings.py
+++ BookingRoom_Django/settings.py
@@ -76,16 +76,24 @@ WSGI_APPLICATION = 'BookingRoom_Django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookingroom',
-        'USER': 'luisdgr',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'bookingroom',
+            'USER': 'tu_usuario',
+            'PASSWORD': 'tu_password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
